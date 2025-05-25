ad0d71d6c89b090782c836751d83abcc7c3ac468
felipe <felipe.baleche2@gmail.com>
2025-05-25 19:55:56 -0300

    add basic configuration through cli args

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,29 @@
+use clap::Parser;
 use server::Result;
 use server::Server;
 
 mod macros;
 mod server;
 
-const ADDR: &str = "127.0.0.1:8080";
+#[derive(clap::Parser)]
+#[command(version, about = "Real time multi-user chat backend", long_about = None)]
+struct Cli {
+    #[arg(
+        long,
+        short,
+        help = "Network PORT used by the server", 
+        value_parser = clap::value_parser!(u16).range(0..),
+        default_value_t = 8080
+    )]
+    port: u16,
+}
 
 fn main() -> Result<()> {
-    let mut server = Server::build(ADDR)?;
+    let cli = Cli::parse();
+    let mut addr = String::from("127.0.0.1:");
+    //push the port to the addr
+    addr.push_str(&cli.port.to_string());
+    let mut server = Server::build(addr)?;
     server.run()?;
     Ok(())
 }
